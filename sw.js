1ce5735836b2067e01c95a092e985f44bf796b67
ozlphrt <ozalph@gmail.com>
2026-02-01 12:35:53 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'flagtest2-v1.0.0';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Force update
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
